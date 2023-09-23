9875c5e5fcad7123921252b5be8fef226f9ca2b8
Your Name <you@example.com>
2023-09-23 02:11:25 -0700

    react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='tab-8 bg-pink-500 text-white font-mono font-extrabold mx-0 py-5'>Dynamic Plugins</h1>
+    </div>
+  );
+}
+
+export default App;
